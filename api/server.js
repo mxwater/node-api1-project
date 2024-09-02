d2cbde8100ca0f66e7c7c53985a737e4faba11d1
“Mariana <marysiq@hotmail.com>
2024-09-02 15:39:05 -0700

    7 tests passing

--- api/server.js
+++ api/server.js
@@ -1,7 +1,35 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
 const User = require('./users/model')
+
 const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json ({
+            message:"Please provide name and bio for the user",
+        })
+    } else {
+        User.insert(user)
+        .then(stuff => {
+
+            res.status(201).json(createdUser)
+        })
+
+        .catch(err => {
+            res.status(500).json({
+                message:'error creating user',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+        
+
+    }
+    User.insert(user)
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -18,13 +46,18 @@ server.get('/api/users', (req, res) => {
 })
 
 server.get('/api/users/:id', (req, res) => {
-    User.findById(req, params.id)
+    User.findById(req.params.id)
     .then(user => {
+        if(!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }
         res.json(user)
     })
     .catch(err => {
         res.status(500).json({
-            message:'error getting user',
+            message:'Error getting user',
             err: err.message,
             stack: err.stack,
         })
